c6a45275cac6b9303050dbfdd1ba761db00db097
Shanna Graves <sg0989463@otc.edu>
2023-12-05 13:16:08 -0600

    Added footer and  nav to js

---
+++ scripts.js
@@ -0,0 +1,2 @@
+document.getElementById("footer").innerHTML = "<footer> Shanna Graves &copy 2023 </footer>";
+document.getElementById("navBar").innerHTML = "<nav><ul><li><a href='index.html'>Home</a></li><li><a href='courses.html'>Courses</a></li><li><a href='hobbies.html'>Hobbies</a></li><li><a href='contac.html'>Contact</a></li></ul></nav>"
