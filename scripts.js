efa31a01b2e8353efbb63aa402da3a78c0ed7f86
Shanna Graves <sg0989463@otc.edu>
2023-12-07 07:53:10 -0600

    Added media query, validated code

--- scripts.js
+++ scripts.js
@@ -1,2 +1,4 @@
+// Footer 
 document.getElementById("footer").innerHTML = "<footer> Shanna Graves &copy 2023 </footer>";
-document.getElementById("navBar").innerHTML = "<nav><ul><li><a href='index.html'>Home</a></li><li><a href='courses.html'>Courses</a></li><li><a href='hobbies.html'>Hobbies</a></li><li><a href='contact.html'>Contact</a></li></ul></nav>"
+// Nav Bar
+document.getElementById("navBar").innerHTML = "<nav><ul><li><a href='index.html'>Home</a></li><li><a href='courses.html'>Courses</a></li><li><a href='hobbies.html'>Hobbies</a></li><li><a href='contact.html'>Contact</a></li></ul></nav>";
